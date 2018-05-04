850ca8d479f60685b48dfff4e61d53dcd89ff18b
Marcelo Axel <axelmar@github.com>
2018-05-04 15:17:36 -0300

    Abrindo arquivo e lendo arquivo

--- codificacao.asm
+++ codificacao.asm
@@ -1,47 +1,48 @@
 	.data
 	msg1: .asciiz "Digite o nome do arquivo a ser codificado:"
+	arquivo: .space 20
+	buffer: .space 1024
+	erro: .asciiz "Nao foi possivel abrir o arquivo"
 	data1: .asciiz "data1.txt"
 	
 	.text	
 	.globl main
 
 main:
-	#print msg1
+	#Mensagem para receber nome do arquivo
 	li	$v0, 4
 	la	$a0, msg1
 	syscall
 	
-	#get n from user and save
-	#li	$v0, 8
-	#la	$a0, ($v0)
+	#Pega o nome do arquivo  #----------------- da erro na hora de abrir o arquivo
+	li	$v0, 8
+	la	$a0, arquivo
+	li	$a1, 20
+	syscall
 	
-	#li	$a1, 20
-	#syscall
-	#move	$t0, $v0
-	#beqz 	$t0, exit
-	
-	#li $v0, 4
-	#move $a0, $t0
+	################################################################## ATE AKI ESTA TUDO CERTO
 
+	#Abre o arquivo para compilação
 	li   $v0, 13      # system call for open file
 	la   $a0, data1     # output file name
-	li   $a1, 1        # Open for writing (flags are 0: read, 1: write)
-	li   $a2, 100        # mode is ignored
+	li   $a1, 0        # Open for writing (flags are 0: read, 1: write)
+	li   $a2, 0        # mode is ignored
 	syscall            # open a file (file descriptor returned in $v0)
 	move $s6, $v0      # save the file descriptor
+	bltz $s6, error
 	
-  ###############################################################
-  # Write to file just opened
-  li   $v0, 14       # system call for write to file
-  move $a0, $s6      # file descriptor 
-  la   $a1, data1	     # address of buffer from which to write
-  li   $a2, 100       # hardcoded buffer length
-  syscall            # write to file
-	 move $s1, $v0
+	#Le o arquivo
+	li $v0, 14	# system call for read from file
+	move $a0, $s6	# file descriptor
+	la $a1, buffer	# endereço do input buffer
+	li $a2, 1024	# numero de caracteres a serem lidos
+	syscall
 	
-	 li $v0, 4
-	 move $a0, $s1
-	 syscall
 	
+	
+		
+error: li $v0, 4
+	la $a0, erro
+	syscall		
 	
 exit:
